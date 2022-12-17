4c77dc574412c6b5fc50cf6deb285b950ae8a663
Hieu Nguyen Duc <isaactesla@gmail.com>
2022-12-17 15:14:15 +0000

    add p10k

--- .zshrc
+++ .zshrc
@@ -9,10 +9,9 @@ export TERM="xterm-256color"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="codespaces"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME="codespaces"
 # a theme from this variable instead of looking in $ZSH/themes/
 # If set to an empty array, this variable will have no effect.
 # ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
@@ -118,6 +117,8 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+source ~/.p10k.zsh
+
 export EDITOR=nano
 
 alias fdir="find . -type d -name"
--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo -e "\u001b[7m Installing dot files...\u001b[0m"
 cat .gitconfig >> $HOME/.gitconfig
 cat .zshrc > $HOME/.zshrc
 cat .tmux.conf > $HOME/.tmux.conf
+cat .p10k.zsh > $HOME/.p10k.zsh
 
 echo -e "\u001b[7m Done. Please restart terminal!\u001b[0m"
