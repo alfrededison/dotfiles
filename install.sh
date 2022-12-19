03e9687b4ca44a3a8b8e97a792c3c3284454b514
Nguyen Duc Hieu <isaactesla@gmail.com>
2022-12-19 11:22:58 +0700

    update installer

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo -e "\u001b[7m Installing dot files...\u001b[0m"
+cat .gitattributes >> $HOME/.gitattributes
 cat .gitconfig >> $HOME/.gitconfig
 cat .zshrc > $HOME/.zshrc
 cat .tmux.conf > $HOME/.tmux.conf
