ee7bd7a0e0303da8759b86fa73f83879460b4dc7
Nguyen Duc Hieu <isaactesla@gmail.com>
2023-02-08 10:11:52 +0700

    overwrite .gitconfig

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo -e "\u001b[7m Installing dot files...\u001b[0m"
-cat .gitconfig >> $HOME/.gitconfig
+cat .gitconfig > $HOME/.gitconfig
 cat .zshrc > $HOME/.zshrc
 cat .tmux.conf > $HOME/.tmux.conf
 cat .p10k.zsh > $HOME/.p10k.zsh
