89288af4fe412f9e95c395439f28cfb50f665199
Hieu Nguyen Duc <isaactesla@gmail.com>
2023-03-27 15:12:17 +0700

    update script to detect gitconfig

--- install.sh
+++ install.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-echo -e "\u001b[7m Cloning zsh plugins...\u001b[0m"
+echo '>>>>>>>>>> Cloning zsh plugins...'
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/alias-tips
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo -e "\u001b[7m Installing dot files...\u001b[0m"
-cat .gitconfig > $HOME/.gitconfig
-cat .zshrc > $HOME/.zshrc
-cat .tmux.conf > $HOME/.tmux.conf
-cat .p10k.zsh > $HOME/.p10k.zsh
+echo '>>>>>>>>>> Installing ZSH configs...'
+cat .zshrc >$HOME/.zshrc
+cat .p10k.zsh >$HOME/.p10k.zsh
 
-echo -e "\u001b[7m Done. Please restart terminal!\u001b[0m"
+echo '>>>>>>>>>> Updating git alias...'
+gitconfig_path="$HOME/.gitconfig"
+
+# Check if the file exists
+if [ -f "$gitconfig_path" ]; then
+  echo "$gitconfig_path exists."
+  if grep -q "\[alias\]" "$gitconfig_path"; then
+    echo "$gitconfig_path contains [alias]. Skipped!"
+  else
+    cat .gitconfig >>$gitconfig_path
+    echo '>>>>>>>>>> Added alias to .gitconfig!'
+  fi
+else
+  cat .gitconfig >$gitconfig_path
+  echo '>>>>>>>>>> Copied .gitconfig!'
+fi
+
+echo '>>>>>>>>>> Installing tmux configs...'
+cat .tmux.conf >$HOME/.tmux.conf
+
+echo '>>>>>>>>>> Done. Please restart terminal! <<<<<<<<<<'
